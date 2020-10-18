9d1d31ad6ef4aa91775417ad23dc49fa416cdba0
Aubrey Isaacman <Aubs@iMac.local>
2020-10-18 12:29:47 -0700

    first commit with unity project

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* created by Aubrey Isaacman
+ * 
+ * This script lets the player kiss and stop kissing their partner
+ *      If their partner's lips are in the kiss position and the player presses the kiss button, kiss successful
+ *      If their partner's lips are resting and the player presses the button, kiss is UNsuccessful
+ *      If their partner's lips are in the kiss position and the player does NOT press the button, kiss is UNsuccessful
+ * 
+ */
+
+public class PlayerControls : MonoBehaviour
+{
+
+    public int liptype; // I need a way to change THIS VAR with whatever the array element is
+    // need to reference this line from lipspawner script:         GetComponent<SpriteRenderer>().sprite = Sprite_Pic[rand];
+    public Sprite[] spriteRef;
+
+    public LipScript lipspawner;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        liptype = gameObject.GetComponent<lipspawner>().rand;
+        //spriteRef = gameObject.GetComponent<lipspawner>().Sprite_Pic[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(spriteRef[liptype] == spriteRef[0])
+        {
+            Debug.Log("partner is KISSING");
+        } else if (liptype == 1)
+        {
+            Debug.Log("partner is RESTING");
+        } else {
+            Debug.Log("error");
+        }
+
+
+        // // check which sprite is active on the screen
+        // --> you'll probably need to call the other script to do this
+            // check if the Arduino button is being pressed
+            // Then, play kiss sound and spawn hearts
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Player smooch");
+        } else if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("Stop kiss");
+        }
+
+    }
+
+    // look at the array of lips
+    // look at this for help https://www.reddit.com/r/Unity3D/comments/8lseyt/accessing_an_object_from_2d_array_from_another/dzi8z0u/
+    void DoSomething()
+    {
+        //Sprite[] lipsSprites = lipspawner.instance.Sprite_Pic(this);
+    }
+    
+}
---
+++ Assets/Scripts/lipspawner.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* created by Aubrey Isaacman
+ * 
+ * This script will randomly change the lips image every random number of seconds
+ * 
+ * Following this YouTube tutorial: https://www.youtube.com/watch?v=HxyGY-xDKas&ab_channel=LemauDevTutorialChannel
+ */
+
+public class lipspawner : MonoBehaviour
+{
+    public int rand;
+
+    public Sprite[] Sprite_Pic;
+
+    // trying to give PlayerControls access to this script and the array of lips
+    // https://www.reddit.com/r/Unity3D/comments/8lseyt/accessing_an_object_from_2d_array_from_another/dzi8z0u/
+    // option 3
+    public static LipScript instance;
+
+
+    // for waiting random seconds before changing sprite
+    public int min = 0;
+    public int max = 10;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Change());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+
+    IEnumerator Change()
+    {
+        //Debug.Log("entered coroutine");
+
+        // wait a random amount of time before changing the sprite
+        int wait_time = Random.Range(min, max);
+
+        // change the sprite
+        rand = Random.Range(0, Sprite_Pic.Length);
+        GetComponent<SpriteRenderer>().sprite = Sprite_Pic[rand];
+
+        // return a new wait time
+        yield return new WaitForSeconds(wait_time);
+
+        // call itself again so it just keeps on going
+        StartCoroutine(Change());
+
+    }
+}
